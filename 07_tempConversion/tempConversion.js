53e8e3501418bcf0e5cd0a666bb64b3c6fa567c9
Roxana <ileanaroxanamitroi@gmail.com>
2024-03-14 00:52:19 +0200

    Add exercies 5-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return roundToOneDec((temp - 32) * 5 / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return roundToOneDec(temp * 9 / 5 + 32);
 };
+
+function roundToOneDec(n) {
+  return Math.round(n * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
