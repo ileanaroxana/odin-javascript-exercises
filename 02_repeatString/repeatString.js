8305f0c3ef10b7de622e2ca38106984d30f02260
Roxana <ileanaroxanamitroi@gmail.com>
2024-03-14 00:08:34 +0200

    Add exercises 1-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, nrOfRepetitions) {
+    if (nrOfRepetitions < 0) 
+        return 'ERROR';
+    
+    let result = '';
+    for (let i = 0; i < nrOfRepetitions; i++)
+        result += string;
 
+    return result;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const result = [];
 
+    array.forEach((item) => {
+        if (!args.includes(item))
+            result.push(item);
+    });
+
+    return result;
 };
 
 // Do not edit below this line
